d829480ba5e1906a6637f962baf9baac18fd9b42
wrealaero <clanwarobi@gmail.com>
2025-03-10 17:19:14 -0500

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -11,7 +11,7 @@ end
 
 local function downloadFile(path, func, overrideRepo)
 	if not isfile(path) then
-		local repo = overrideRepo or 'QP-Offcial/VapeV4ForRoblox'
+		local repo = overrideRepo or 'wrealaero/QpSkidV4'
 		local suc, res = pcall(function()
 			return game:HttpGet('https://raw.githubusercontent.com/'..repo..'/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
@@ -44,7 +44,7 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/QP-Offcial/VapeV4ForRoblox')
+		return game:HttpGet('https://github.com/wrealaero/QpSkidV4')
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
